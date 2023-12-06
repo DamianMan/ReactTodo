dd8d608b2f5d1e70e9c812d7fe7f3e70539f6f52
DamianMan <122468149+DamianMan@users.noreply.github.com>
2023-12-06 16:01:52 +0100

    I commited src/App.js src/App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Form from './Form';
+import ShoppingList from './ShoppingList';
+import Box from './Box';
+import BoxList from './BoxList';
+import NewBoxForm from './NewBoxForm';
+import TodoList from './TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Form /> */}
+      {/* <ShoppingList /> */}
+      {/* <BoxList /> */}
+      <TodoList />
     </div>
   );
 }
