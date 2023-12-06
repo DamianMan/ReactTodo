21d759200856815a0fa7ad0c4db6a0f4ae8adc62
DamianMan <122468149+DamianMan@users.noreply.github.com>
2023-12-06 16:03:19 +0100

    I commited src/App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import Form from './Form';
-import ShoppingList from './ShoppingList';
-import Box from './Box';
-import BoxList from './BoxList';
-import NewBoxForm from './NewBoxForm';
+
 import TodoList from './TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Form /> */}
-      {/* <ShoppingList /> */}
-      {/* <BoxList /> */}
+
       <TodoList />
     </div>
   );
